01f7d5fae215cc95a3e61957e01fbf3e4144452b
Sudharsan G S <g.s.sudharsan99@gmail.com>
2020-04-10 17:10:51 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const APP_ID = 'd4b62089';
+    const APP_KEY = 'd4b62089';
+
+    return(
+        <div>
+
+        </div>
+    )
 }
 
 export default App;
